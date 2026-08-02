cbe0abbf72ca4f1d999d7a174e2dc6a2d3a4a037
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-02 15:35:45 +0530

    -- c# hello world

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,3 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("This is my first  C# learning program.");
+Console.WriteLine("I am Mahammedaadil and i am learning c# programming language.");
