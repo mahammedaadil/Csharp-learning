6c9985c9ec719f3a54e349c63dcb51372b1ac3da
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-02 15:50:51 +0530

    -- first odd even program

---
+++ oddEven/Program.cs
@@ -0,0 +1,21 @@
+﻿public class oddEven { 
+ public static void Main(string[] args)
+    {
+        int number;
+        Console.WriteLine("Enter a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("{0} is an even number.", number);
+        }
+        else
+        {
+            Console.WriteLine("{0} is an odd number.", number);
+        }
+
+        Console.WriteLine("end of oddEven");
+
+    }
+
+}
+
