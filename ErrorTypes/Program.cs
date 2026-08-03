84430494a05a7afec170d531c53c4cccfca2dc36
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-03 18:44:02 +0530

    -- learned different type of errors runtime,compile time,and logical errors

---
+++ ErrorTypes/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Checking Errors !");
+
+//Compile Time
+
+//Console.WriteLine("Hello)  some errors like forget semicolon or quote called compile time or syntax errors
+
+
+//run time
+
+//int[] a = { 1, 2, 3, 4 };
+
+//int index = int.Parse(Console.ReadLine());
+
+//Console.WriteLine(a[index]); //index is out of array boundary , run time error
+
+//ex-2 
+
+int num1;
+int num2;
+Console.WriteLine("Enter first number for divison");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number for divison");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+int division = num1 / num2;
+Console.WriteLine("divsion is : {0}", division);
+
+
+//logical errors 
+
+int c = 20 - 10 + 10 * 10 / 2; //logical priority type error any mistake in logic is caled logical errors
+
+
+
+
