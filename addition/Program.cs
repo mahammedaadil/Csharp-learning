86837dcb723934e3278abfb2e651ad4f6f731ad2
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-03 13:29:58 +0530

    -- written addition program

---
+++ addition/Program.cs
@@ -0,0 +1,18 @@
+﻿public class addition
+{
+    public static void Main (string[] args)
+    {
+
+        int a;
+        int b;
+        int c;
+        Console.WriteLine("enter first number");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number ");
+        b = Convert.ToInt32(Console.ReadLine());
+        c = a + b;
+        Console.WriteLine("addition : {0}", c);
+
+    }
+
+}
