bfc824888e3f472e4bbf04f9acda0a31d5ce2015
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-03 17:22:02 +0530

    -- learned writeline , write methods and escape sequences

---
+++ WriteLineMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Today is Monday");
+Console.WriteLine("Am at Work");
+Console.Write("Write");
+Console.Write("Line");
+
+
+//Escape Sequences
+
+//double quotes
+Console.WriteLine("Hello \"Aadil\"");
+
+//tab
+Console.WriteLine("Hello\tTotal");
+
+//new line
+Console.WriteLine("Name:Aadil\nAge:25");
+
+//backslash
+Console.WriteLine("Hello\\Patel\\Total");
