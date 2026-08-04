b1833e4414272b9cb87039fa61f4df0a1e321d24
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-04 15:47:54 +0530

    -- learned about ReadLine method and if,switch

---
+++ IfBlocksAndSwitchStatement/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Enter your age to check eligibility:");
+int age = int.Parse(Console.ReadLine());
+
+//if else if,else
+
+//if (age >= 18)
+//{
+//    Console.WriteLine($"u are {age},u can vote");
+//}
+//else if (age <= 0)
+//{
+//    Console.WriteLine("Invalid age");
+//    return;
+//}
+//{
+//    Console.WriteLine("Sorry,u cannot vote");
+//}   //works on boolean values 
+
+
+
+//switch
+switch (age)
+{
+    case   >= 0 and  <= 10:
+        Console.WriteLine("You are under 10 or 10");
+        break;
+
+    case  >= 10 and  <= 20:
+        Console.WriteLine("You are under 20 or 20");
+        break;
+    default:
+        Console.WriteLine("Your age is greater than 20");
+        break;
+}
---
+++ readlineWithTypeCasting/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter First Number:");
+int a = int.Parse(Console.ReadLine()); //this is way to explicit type convertion if null will give error
+Console.WriteLine("Enter Second Number:");
+int b = Convert.ToInt32(Console.ReadLine()); //another way we can take integer length  as per our need
+
+int c = a / b;
+Console.WriteLine(c);
