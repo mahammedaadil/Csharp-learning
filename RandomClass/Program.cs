22c8fc54286c4d334c57d0c48f9647a13ba25410
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-04 17:54:06 +0530

    -- random class learning

---
+++ RandomClass/Program.cs
@@ -0,0 +1,7 @@
+﻿Random ran = new Random();
+
+
+Console.WriteLine(ran.Next(1, 10)); //GENERATE num between 1-10
+Console.WriteLine(ran.Next(100));// GENERATE  num between 1-100
+Console.WriteLine(ran.NextDouble()); //float number 
+
