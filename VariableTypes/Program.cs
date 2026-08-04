05a3a073e799654da54579cf5e2559eab2ae7dd3
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-04 12:41:29 +0530

    -- learned about variables,concatanation ,interpolation and  type conversion

---
+++ VariableTypes/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+//whole type
+
+int a; //declaration
+a = 10; //intialization
+
+
+//double --> float type values
+
+double b;
+b = 10.5;
+
+//boolean
+
+bool isAvailable = true;
+
+//char
+char c = 'A'; // only single character allowed 
+
+//string 
+
+string d = "hello guys"; //multiple chars allowed
+
+
+
+//concatanation
+
+string e = "hello";
+string f = e + " world";
+
+Console.WriteLine(f);
+
+//implicit type conversion
+int g = 10;
+Console.WriteLine("hello" + g); // automatic converts g into string ,performed by compiler
+
+//explicit type conversion
+Console.WriteLine("hello" + g.ToString()); //explicitely convert by self
+
+//interpolation
+Console.WriteLine($"hello {g}");
+
