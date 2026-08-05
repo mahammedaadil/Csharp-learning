e470d17ae95a18c3afc0a1633d70391eea260480
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-05 11:54:22 +0530

    --  learned break and continue keywords

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int number = 0;
+
+while (number <= 10)
+{
+
+    //if (number == 3)
+    //{
+    //    number++;
+    //    continue;
+    //} //skips the condition
+
+    if (number == 3)
+    {
+        break;
+    } //terminate loop
+
+    Console.WriteLine(number);
+    number++;
+
+} 
