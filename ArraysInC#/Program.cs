84d397ff4e9f0af6e6e89e7041a04102b6aa5712
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-05 13:49:01 +0530

    -- learned about one,two,jagged array types in c# and understood how we can print those using for loop

--- ArraysInC#/Program.cs
+++ ArraysInC#/Program.cs
@@ -1,24 +1,78 @@
-﻿//declaration of array
+﻿//one diamentional array
 
-int[] arr = new int[10];
-arr = [10, 20, 30]; //intialization
+////declaration of array
 
-Console.WriteLine(arr[1]); //access with index;
+//int[] arr = new int[10];
+//arr = [10, 20, 30]; //intialization
 
-
-//declaration and intialization together
-int[] ex = { 10, 20, 30, 40, 50, 60, 70 };
+//Console.WriteLine(arr[1]); //access with index;
 
 
-for (int i = 0; i <= ex.Length; i++)
+////declaration and intialization together
+//int[] ex = { 10, 20, 30, 40, 50, 60, 70 };
+
+
+//for (int i = 0; i <= ex.Length; i++)
+//{
+//    Console.WriteLine(ex[i]);
+//} //print whole array  
+
+
+
+//// for each loop
+//foreach(int val in ex)
+//{
+//    Console.WriteLine(val);
+//}
+
+
+
+
+//Two Diamentional Arrays
+
+//int[,] ar =
+//{
+//    {100,200,300 },
+//    {300,400,500 },
+//    {300,400,500 }
+
+//}; //declaration and intialization
+
+//Console.WriteLine(ar[0,1]); //access
+
+////loop 2-d array
+//Console.WriteLine(ar.GetLength(0)); //row count
+//Console.WriteLine(ar.GetLength(1)); //column count 
+
+//for(int row=0; row < ar.GetLength(0); row++)
+//{
+//    for(int col = 0; col < ar.GetLength(1); col++)
+//    {
+//        Console.Write(ar[row,col] + " ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+//jaggedarray 
+
+int[][] jArr =
 {
-    Console.WriteLine(ex[i]);
-} //print whole array  
+    [10,20],
+    [10,20,30],
+    [30,40,50,60]
+}; //declaration and intialization
 
- 
+Console.WriteLine(jArr[0].Length); //length of column  by row
+Console.WriteLine(jArr.Length); //length of rows
 
-// for each loop
-foreach(int val in ex)
+//loop through jagged array
+for(int row = 0; row < jArr.Length; row++)
 {
-    Console.WriteLine(val);
+    for(int col = 0; col < jArr[row].Length; col++)
+    {
+        Console.Write(jArr[row][col] + " ");
+    }
+    Console.WriteLine();
 }
+
