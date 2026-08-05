d36f58ae1883de326a326b498e0b88aec460e29b
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-05 11:31:13 +0530

    -- loops in c#

---
+++ Loops/Program.cs
@@ -0,0 +1,24 @@
+﻿//while loop 
+int a = 0;
+while (a <= 10)
+{
+    Console.WriteLine(a);
+    a++;
+}  //check condition first
+
+int b = 10;
+
+//do-while
+do
+{
+    Console.WriteLine(b);
+    b++; 
+} while (b <= 10); //atleast execute one time,check condition at last
+
+
+//for loop
+
+for(int i = 0; i <= 10; i++)
+{
+    Console.WriteLine(i);
+} //mostly used for arrays
