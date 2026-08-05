6ce36c9f576d0d596671fed804a8f76f089e5f66
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-05 11:18:47 +0530

    -- learned datetime and understood how we can calculate dates ,get month,year,hr,etc also date formation.

---
+++ DateStruct/Program.cs
@@ -0,0 +1,26 @@
+﻿
+//current date
+DateTime dt = DateTime.Now;
+
+Console.WriteLine(dt);
+
+//format date
+Console.WriteLine(dt.ToString("dd-mm-yyyy"));
+Console.WriteLine(dt.ToString("hh-mm-ss"));
+
+//get year,month,today,etc
+int year = dt.Year;
+Console.WriteLine(year);
+
+int month = dt.Month;
+Console.WriteLine(month);
+
+int today = dt.Day;
+Console.WriteLine(today);
+
+//calc difference
+DateTime currentDay = DateTime.Today;
+DateTime afterTwoDays = currentDay.AddDays(3);
+
+Console.WriteLine(currentDay);
+Console.WriteLine(afterTwoDays);
