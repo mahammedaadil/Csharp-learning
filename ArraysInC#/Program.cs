b3e8ce8c649acac48517affa737db0c0ea2bad86
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-05 12:01:49 +0530

    -- one diamentional array in c#

---
+++ ArraysInC#/Program.cs
@@ -0,0 +1,16 @@
+﻿//declaration of array
+
+int[] arr = new int[10];
+arr = [10, 20, 30]; //intialization
+
+Console.WriteLine(arr[1]); //access with index;
+
+
+//declaration and intialization together
+int[] ex = { 10, 20, 30, 40, 50, 60, 70 };
+
+
+for (int i = 0; i <= ex.Length; i++)
+{
+    Console.WriteLine(ex[i]);
+} //print whole array  
