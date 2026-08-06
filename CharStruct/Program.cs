86f7423f5d3872ff074d7786233702ddcf4eb881
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-06 20:06:07 +0530

    -- learned about CharStruct in C#

---
+++ CharStruct/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+char test = 'A';
+
+//all methods return true / false ,and works like its names
+Console.WriteLine(char.IsUpper(test)); 
+Console.WriteLine(char.IsLower(test));
+Console.WriteLine(char.IsLetter(test));
+Console.WriteLine(char.IsDigit(test));
+Console.WriteLine(char.IsLetterOrDigit(test));
+Console.WriteLine(char.IsPunctuation(test));
+Console.WriteLine(char.IsWhiteSpace(test)); 
