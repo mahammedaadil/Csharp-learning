db5d5b7be2965a9a38ce70f4dacc740aeea28083
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-08 15:52:24 +0530

    -- understood different method types in c#

---
+++ MethodsInC#/Program.cs
@@ -0,0 +1,41 @@
+﻿//methods without parameters and return values
+
+
+void test()
+{
+    Console.WriteLine("testing method in c#");
+}
+
+test();
+
+//method without parameters but with a return value
+
+
+int a;
+int b;
+
+Console.WriteLine("Enter value of A:");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter value of B:");
+b = Convert.ToInt32(Console.ReadLine());
+
+int test2()
+{
+ 
+    int c = a + b;
+    return c;
+}
+
+
+int result = test2();
+Console.WriteLine(result);
+
+
+//method with parameters and return values
+int test3(int c,int d)
+{
+    return c * d;
+}
+
+
+Console.WriteLine(test3(a,b));
