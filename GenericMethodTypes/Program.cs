b1015af112c7a4d8638e1953e367c6da3b5dc7bc
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-12 16:04:20 +0530

    learned about generic method types + overloading of methods in c# also understool how top-level statments and classic console app do work internally in c#

---
+++ GenericMethodTypes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethodTypes
+{
+    internal class Program
+    {
+
+        static int Print(int a)
+        {
+            return a;
+        }
+
+        static string Print(string s)
+        {
+            return s;
+        }
+
+        //do same thing with generic type
+
+        static T PrintF<T>(T m)
+        {
+            return m;
+        }
+
+
+        //with multiple parameters 
+        static void PrintFC<T,U>(T p,U q,U w)
+        {
+            Console.WriteLine($"{p},{q},{w}");
+        }
+        static void Main(string[] args)
+        {
+
+            int r= Print(10);
+            Console.WriteLine(r);
+
+            string r1 = Print("aadil");
+            Console.WriteLine(r1);
+
+            // the type get check at run time
+            Console.WriteLine(PrintF("C"));
+            Console.WriteLine(PrintF(10));
+            Console.WriteLine(PrintF(10.5));
+
+
+            PrintFC("aadil", 1,1);
+
+        }
+    }
+}
