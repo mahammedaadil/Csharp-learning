7eee8f9c613fdd3df96e77433b708f9a9a63f6c3
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-13 17:54:36 +0530

    -- understood about collection of object , how we can use other classes properties and how we can create list using classType

---
+++ CollectionOfObjects/Display.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Ecommerce;
+
+namespace CollectionOfObjects
+{
+    internal class Display
+    {
+
+        public static void Main()
+        {
+            List<Product> products = new List<Product>();
+
+            string choice;
+            do
+            {
+                Console.WriteLine("Enter Product Id:");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Product Name:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter Product Price:");
+                double price = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Product Manufecture Date (YYYY-MM-DD):");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+
+
+                Product product = new Product() { ProductId = id, ProductName = name, ProductPrice = price, ProductManufectureDate = date };
+                products.Add(product);
+
+                Console.WriteLine("Product Is Added !");
+                Console.WriteLine("Do You Want To Add More ? Press Enter");
+
+                choice = Console.ReadLine().ToUpper();
+                
+            } while (choice != "STOP");
+
+            Console.WriteLine("\nProducts Are:");
+            foreach(Product item in products)
+            {
+                Console.WriteLine("Product Id:"+item.ProductId);
+                Console.WriteLine("Product Name:" + item.ProductName);
+                Console.WriteLine("Product Price:" + item.ProductPrice);
+                Console.WriteLine("Product Manufecture Date :" + item.ProductManufectureDate);
+            }
+        }
+    }
+}
---
+++ CollectionOfObjects/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Ecommerce
+{
+    internal class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+
+        public double ProductPrice { get; set; }
+
+        public DateTime ProductManufectureDate { get; set; }
+
+    }
+}
