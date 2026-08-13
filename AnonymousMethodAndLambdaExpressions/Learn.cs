c0e89a205524cc123bdcb4880ae13a21cf1c3cb3
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-13 15:00:48 +0530

    -- understood about lambda expressions and anonymous methods in c# also how we can use it wwith delegate keyword,

---
+++ AnonymousMethodAndLambdaExpressions/Learn.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using AnonymousMethodAndLambdaExpressions;
+
+namespace LearningMethods
+{
+    internal class Learn
+    {
+        public static void Main()
+        {
+            Program p = new Program();
+            Console.WriteLine(p.My(10,20));
+            Console.WriteLine(p.MyLambda(10,10));
+        }
+    }
+}
---
+++ AnonymousMethodAndLambdaExpressions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace AnonymousMethodAndLambdaExpressions
+{
+    public delegate int MyDelegate(int a, int b);
+    internal class Program
+    {
+       public MyDelegate My = delegate (int a, int b)
+        {
+            return a + b;
+        }; //anonymous function.
+
+
+        public MyDelegate MyLambda = (int a, int b) =>
+        {
+            return a * b;
+        }; //lambda expression.
+
+
+    }
+}
