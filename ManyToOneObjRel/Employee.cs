cf90b862f664ca08f6e673fc75e95e87bbb44afd
mahammedaadil patel <adilchoice30@gmail.com>
2026-08-14 18:40:17 +0530

    -- got  knowledge of a ManyToOne RelationShip inb obj relations

---
+++ ManyToOneObjRel/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManyToOneObjRel;
+
+namespace EmpSpace
+{
+    internal class Employee
+    {
+        public string EmpName { get; set; }
+        public double EmpSalary { get; set; }
+
+        public Department dept { get; set; }
+
+       
+    }
+}
---
+++ ManyToOneObjRel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using EmpSpace;
+
+namespace ManyToOneObjRel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee emp1 = new Employee() { EmpName = "aadil", EmpSalary = 2000 };
+            Employee emp2 = new Employee() { EmpName = "sahil", EmpSalary = 3000 };
+
+            Department deptInit = new Department() { DepartmentId = 1, DepartmentName = "Account" };
+
+            emp1.dept = deptInit;
+            emp2.dept = deptInit;
+
+            Console.WriteLine($"EmpName:{emp1.EmpName} EmpSalary:{emp1.EmpSalary} EmpDept:{emp1.dept.DepartmentName}");
+            Console.WriteLine($"EmpName:{emp2.EmpName} EmpSalary:{emp2.EmpSalary} EmpDept:{emp2.dept.DepartmentName}");
+
+        }
+    }
+}
